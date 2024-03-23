bc5a37ee4a4f8d7e8e8838ac505fd8fe7e2d5969
amym114 <amylcollins@gmail.com>
2024-03-23 12:38:35 -0500

    More /target route files

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+[
+  {"Target 1", 1.2953493105364986, 103.85826194711629},
+  {"Target 2", 1.2946628404229832, 103.86017167980194},
+  {"Target 3", 1.2947915535834391, 103.85798299739815},
+  {"Target 4", 1.2935473261018144, 103.8506659312996}
+]
+|> Enum.map(fn {name, lat, lon} ->
+  now = DateTime.utc_now(:second)
+
+  %{
+    name: name,
+    inserted_at: now,
+    updated_at: now,
+    lat: lat,
+    lng: lon
+  }
+end)
+|> then(&Notjazzfest.Repo.insert_all(Notjazzfest.Targets.Target, &1, on_conflict: :nothing))
