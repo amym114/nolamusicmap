d8e202da588fb0809655e8d4af06d2701a50815b
amym114 <amylcollins@gmail.com>
2024-03-25 11:04:29 -0500

    Adds Elixir Crawly and initial Event Spider

---
+++ lib/notjazzfest/spiders/event_spider.ex
@@ -0,0 +1,69 @@
+defmodule NotJazzfest.Spider.EventSpider do
+  use Crawly.Spider
+  require Logger
+
+  @impl Crawly.Spider
+  def base_url(), do: "https://www.wwoz.org/"
+
+  @impl Crawly.Spider
+  def init() do
+    Logger.warning("HEY GIRL INIT")
+    [start_urls: ["https://www.wwoz.org/calendar/livewire-music?date=2024-04-26"]]
+  end
+
+  @impl Crawly.Spider
+  def parse_item(response) do
+    Logger.warning("HEY GIRL PARSIN'")
+
+    # Parse response body to document
+    {:ok, document} = Floki.parse_document(response.body)
+
+    all_shows =
+      document
+      |> Floki.find(".livewire-listing")
+      |> Floki.find(".panel")
+      |> Enum.map(fn venue ->
+        %{
+          venue_name:
+            String.trim(Floki.find(venue, ".panel-heading .panel-title a") |> Floki.text()),
+          events:
+            venue
+            |> Floki.find(".panel-body")
+            |> Floki.find(".row")
+            |> Enum.map(fn row ->
+              %{
+                name:
+                  String.trim(
+                    Floki.find(row, "p.truncate")
+                    |> Floki.text()
+                  ),
+                date_and_time:
+                  String.trim(
+                    Floki.find(row, "p:last-of-type")
+                    |> Floki.text()
+                  )
+              }
+            end),
+          venue_url: Floki.find(venue, ".panel-heading .panel-title a") |> Floki.attribute("href")
+        }
+      end)
+
+    all_shows
+    |> Enum.map(fn show_venue ->
+      Logger.info("\n\nShow Venue: ")
+      Logger.warning(inspect(show_venue.venue_name))
+      Logger.info(inspect(show_venue.venue_url))
+
+      show_venue.events
+      |> Enum.map(fn show ->
+        Logger.info(inspect(show.name))
+        Logger.info(inspect(show.date_and_time))
+      end)
+    end)
+
+    # Logger.warning(inspect(all_shows))
+    # %Crawly.ParsedItem{}
+    %Crawly.ParsedItem{items: all_shows, requests: []}
+    # %Crawly.ParsedItem{items: all_shows, requests: next_requests}
+  end
+end
--- mix.exs
+++ mix.exs
@@ -39,7 +39,6 @@ defmodule Notjazzfest.MixProject do
       {:phoenix_html, "~> 4.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20.2"},
-      {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
@@ -57,7 +56,9 @@ defmodule Notjazzfest.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:crawly, "~> 0.16.0"},
+      {:floki, ">= 0.33.0"}
     ]
   end
 
